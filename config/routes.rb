1ef0dbdd6dafb485c61f56b369139ba3b480048d
yait-ucess <bandai28@icloud.com>
2021-01-25 17:58:24 +0900

    Add 新規登録/ログイン/ログアウト

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {   registrations: 'users/registrations',
+    sessions: 'users/sessions' }
   root to: "devs#index"
 end
