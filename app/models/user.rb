131f1050e15f2805d3a21f92347ef80381bcd290
yait-ucess <bandai28@ezweb.ne.jp>
2021-01-27 15:36:24 +0900

    Add Validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :nickname, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i.freeze
+  validates :email, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false } #Email大・小文字の区別なし、一意性
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX } # パスワード半角英数字
+       
 end
