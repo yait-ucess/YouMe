ec5d7e622faab9bb2d903c554fe0d74b8c414043
yait-ucess <bandai28@ezweb.ne.jp>
2021-01-29 23:55:45 +0900

    Add profile register

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,13 @@
 class Profile < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  validates :user_id, uniqueness: true
+
+  with_options presence: true do
+    validates :name
+    validates :store_name
+    validates :store_url
+    validates :text
+  end
 end
