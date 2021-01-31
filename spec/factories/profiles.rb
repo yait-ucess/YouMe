d7417a533137c2f0d8fb12b199f09fb44328e237
yait-ucess <bandai28@ezweb.ne.jp>
2021-01-31 17:17:35 +0900

    Add Test unit profile model
    プロフィールモデルの単体テスト

--- spec/factories/profiles.rb
+++ spec/factories/profiles.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :profile do
-    
+    name       { "佐藤ましろ" }
+    store_name { "イタリアン 〜mashiro〜" }
+    store_url  { "https://example.com" }
+    text       { "心よりお待ちしております。" }
+
+    association :user
+
+    after(:build) do |profile|
+      profile.image.attach(io: File.open('app/assets/images/camera.png'), filename: 'camera.png')
+    end
   end
 end
