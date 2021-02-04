e49b7c1268ebdba49d4681f776f67d9bd710f7eb
yait-ucess <bandai28@ezweb.ne.jp>
2021-02-04 12:20:40 +0900

    Add Edit articles
    記事編集機能

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show, :edit, :update]
 
   def index
     @articles = Article.includes(:profile).order("created_at DESC")
@@ -19,11 +20,27 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    @profile = current_user.profile
+    @article.update(article_params)
+    if @article.save
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
   def article_params
     params.require(:article).permit(:image, :title, :text).merge(profile_id: @profile.id)
+  end
+
+  def find_article
+    @article = Article.find(params[:id])
   end
 end
