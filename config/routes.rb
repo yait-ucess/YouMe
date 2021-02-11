dfffd76b977294a271fd71d9ec9b7239b6c44246
yait-ucess <bandai28@ezweb.ne.jp>
2021-02-11 14:08:00 +0900

    Add routes nest likes in profile
    ルーティングのネスト（親がprofile子がlikes）

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {   registrations: 'users/registrations',
     sessions: 'users/sessions' }
   root to: "articles#index"
-  resources :profiles, only: [:new, :create, :show, :edit, :update]
+  resources :profiles, only: [:new, :create, :show, :edit, :update] do
+    resource :likes, only: [:create, :destroy]
+  end
   resources :articles do
     get :search, on: :collection
   end
