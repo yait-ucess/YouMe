92a2814ae9cc4b041b04a198a4caa89a64eab663
yait-ucess <bandai28@ezweb.ne.jp>
2021-02-22 23:57:59 +0900

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   root to: "articles#index"
   devise_for :users
-  resources :profiles, only: [:new, :create, :show, :edit, :update] do
+  resources :users do
     resource :gifts, only: [:new, :create]
   end
+  resources :profiles, only: [:new, :create, :show, :edit, :update]
   resources :articles do
     get :search, on: :collection
     resource :likes, only: [:create, :destroy]
