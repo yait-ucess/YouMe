fdf62e3e38a0e09ed37ade6d9aeec9770a8a49c8
yait-ucess <bandai28@ezweb.ne.jp>
2021-03-05 20:22:07 +0900

    Add users/edit
    ユーザーの編集機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,18 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {   registrations: 'users/registrations',
     sessions: 'users/sessions' }
   root to: "articles#index"
+<<<<<<< Updated upstream
   resources :profiles, only: [:new, :create, :show, :edit, :update]
+=======
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions:      "users/sessions"
+  }
+  #resource :users, only: [:show]
+  resources :profiles, only: [:new, :create, :show, :edit, :update] do
+    resource :gifts, only: [:new, :create, :show]
+  end
+>>>>>>> Stashed changes
   resources :articles do
     get :search, on: :collection
     resource :likes, only: [:create, :destroy]
