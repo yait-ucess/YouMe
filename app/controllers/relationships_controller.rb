a374d5aea8b3cba559c4a32560542c63054c5c2e
yait-ucess <bandai28@ezweb.ne.jp>
2021-03-07 23:29:44 +0900

    Add feature unfollow
     フォローを外す機能追加

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,7 +1,19 @@
 class RelationshipsController < ApplicationController
-  def create
-    @profile = Profile.find(params[:profile_id])
-    @follow = current_user.relationships.create(followed_id: @profile.id)
+  before_action :find_profile, only: [:create, :destroy]
 
+  def create
+    @follow = current_user.relationships.create(followed_id: @profile.id)
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    @unfollow = current_user.relationships.find_by(followed_id: @profile.id)
+    binding.pry
+    @unfollow.destroy
+    redirect_back(fallback_location: root_path)
+  end
+
+  def find_profile
+    @profile = Profile.find(params[:profile_id])
   end
 end
