79254826b586704527e8e907f4ade9b97f381b72
yait-ucess <bandai28@ezweb.ne.jp>
2021-03-08 23:48:11 +0900

    Add action follow-list

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,16 +1,23 @@
 class RelationshipsController < ApplicationController
-  before_action :find_profile, only: [:create, :destroy]
+  before_action :find_profile, only: [:create, :destroy, :show]
 
   def create
-    @follow = current_user.relationships.create(followed_id: @profile.id)
-    redirect_back(fallback_location: root_path)
+    unless current_user == @profile.user
+      @follow = current_user.relationships.create(followed_id: @profile.id)
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def destroy
     @unfollow = current_user.relationships.find_by(followed_id: @profile.id)
-    binding.pry
     @unfollow.destroy
     redirect_back(fallback_location: root_path)
+  end
+
+  def show
+    @my_follow = @profile.user.relationships
+    @follower = @profile.relationships
+    binding.pry
   end
 
   def find_profile
