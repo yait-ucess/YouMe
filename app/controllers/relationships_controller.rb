895b6a15b81e33d51bbe48f7941c90ed3efca701
yait-ucess <bandai28@ezweb.ne.jp>
2021-03-09 12:25:52 +0900

    Fix follow
    フォロー機能のコードリファクタリング

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,5 +1,5 @@
 class RelationshipsController < ApplicationController
-  before_action :find_profile, only: [:create, :destroy, :show]
+  before_action :find_profile, only: [:create, :destroy]
 
   def create
     unless current_user == @profile.user
@@ -12,12 +12,6 @@ class RelationshipsController < ApplicationController
     @unfollow = current_user.relationships.find_by(followed_id: @profile.id)
     @unfollow.destroy
     redirect_back(fallback_location: root_path)
-  end
-
-  def show
-    @my_follow = @profile.user.relationships
-    @follower = @profile.relationships
-    binding.pry
   end
 
   def find_profile
