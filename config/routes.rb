77ed7c7ca031630287cc50de2baad1b7e2638c49
yait-ucess <bandai28@ezweb.ne.jp>
2021-03-09 11:50:14 +0900

    Add routes follow-list
    フォロー、フォロワー一覧表示のルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
   #resource :users, only: [:show]
   resources :profiles, only: [:new, :create, :show, :edit, :update] do
     resource :gifts, only: [:new, :create, :show]
-    resource :relationships, only: [:create, :destroy, :show]
+    resource :relationships, only: [:create, :destroy]
+    member do
+      get :follower, :followed
+    end
   end
 
   resources :articles do
